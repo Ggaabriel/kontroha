3868ca56dacd50c3aac1e1bbf00bba2cf7c1266e
octopussy-ra <djskilleton@gmail.com>
2022-11-26 17:24:52 +0200

    commit8

--- college2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    require __DIR__ . DIRECTORY_SEPARATOR . implode(
-        DIRECTORY_SEPARATOR,
-        explode('\\', str_replace('College', '', $class))
-    ) . '.php';
-});
